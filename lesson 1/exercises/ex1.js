87fa525465913cc718f00c2b94ac8f8e9040f013
sopo <ssop23@yahoo.com>
2021-05-05 16:54:35 +0400

    added new solution

--- lesson 1/exercises/ex1.js
+++ lesson 1/exercises/ex1.js
@@ -1,25 +1,38 @@
 
 function equals(x, y) {
-		if(x===y && Object.is(x,y) && Number.isNaN(x, y) && typeof(x) === typeof(y)) {
-			return false;
-		} else {
-			return true;
-		}
-	}
+const isNegZeroX = checkNegZero(x);
+const isNegZeroY = checkNegZero(y);
+if(isNegZeroX || isNegZeroY) {
+	return isNegZeroX && isNegZeroY;
+}
+
+if(checkNaN(x) && checkNaN(y)){
+	return true;
+}
+	return x === y;
+}
+function checkNegZero(x){
+	return x === 0 && 1 / x == Infinity;
+}	
+function checkNaN(x){
+	return x !== x
+}
+
+
+	console.log(equals("12", 12) === false);
+	console.log(equals(0, -0) === false);
+	console.log(equals(0, NaN) === false);
+	console.log(equals(NaN, 0) === false);
+	console.log(equals(5, "5") === false);
+	console.log(equals("john", "doe") === false);
+	console.log(equals(false, true) === false);
+	console.log(equals(undefined, null) === false);
 	
-	console.log(equals("12", 12));
-	console.log(equals(0, -0));
-	console.log(equals(0, NaN));
-	console.log(equals(NaN, 0));
-	console.log(equals(5, "5"));
-	console.log(equals("john", "doe"));
-	console.log(equals(false, true));
-	console.log(equals(undefined, null));
-	console.log(equals("john", "john"));
-	console.log(equals(-0, -0));
-	console.log(equals(5, 5));
-	console.log(equals(false, false));
-	console.log(equals(null, null));
-	console.log(equals(undefined, undefined));
-	console.log(equals(NaN, NaN));
-	console.log(equals(0, 0));
+	console.log(equals("john", "john") === true);
+	console.log(equals(-0, -0) === true);
+	console.log(equals(5, 5) === true);
+	console.log(equals(false, false) === true);
+	console.log(equals(null, null) === true);
+	console.log(equals(undefined, undefined) === true);
+	console.log(equals(NaN, NaN) === true);
+	console.log(equals(0, 0) === true);
